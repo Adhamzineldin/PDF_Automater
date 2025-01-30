eb2e3537b965c0f45b07580d5d1ad9e252a5d92d
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-01-30 22:02:44 +0200

    progress done

--- app.py
+++ app.py
@@ -150,4 +150,4 @@ def generate_pdf():
 threading.Thread(target=worker, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000, host="0.0.0.0")
