d6656029a4b5e2c97dd0effa06bcf7491d83a7dd
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-02-01 14:58:40 +0200

    running on linux

--- ExcelModifier.py
+++ ExcelModifier.py
@@ -124,6 +124,8 @@ class ExcelModifier:
         # Set print settings to fit on a single page
         sheet.page_setup.fitToWidth = 1
         sheet.page_setup.fitToHeight = 1
+        sheet.page_setup.fitToPage = True
+        
     
         wb.save(excel_path)
 
