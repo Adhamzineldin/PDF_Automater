4df8d37199d3d6165c5e226bd37aa4dad7b111e2
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-02-09 14:50:38 +0200

    fix: update PDF path to use forward slashes for compatibility

--- ACC_Smart_Forms.py
+++ ACC_Smart_Forms.py
@@ -246,7 +246,7 @@ def generate_smart_form():
                 file_name = f"{proj} - {date_now} Summary"
                 pdf_path = f"modified_files/{file_name}.pdf"    
                 modifier.export_to_pdf_no_upload(excel_filename=file_name)
-                acc_api.upload_pdf_to_acc(pdf_path=pdf_path, filename=file_name, folder_name=f"equipment/{proj}")
+                acc_api.upload_pdf_to_acc(pdf_path=pdf_path, filename=file_name, folder_name=f"Equipment/{proj}")
 
             except Exception as e:
                 print(f"Error saving workbook: {e}")
