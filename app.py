6f4c78e4a8e2bbc357ca45f27d2ee065980ca695
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-02-11 10:30:55 +0200

    feat: implement PDF export and save functionality in ACC_Smart_Forms.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def generate_pdf():
 def generate_equipment_form():
     smart_form_object = generate_smart_form()
     
-    return Response("200", status=200, mimetype='text/plain')
+    return Response(status=200)
 
 
 
