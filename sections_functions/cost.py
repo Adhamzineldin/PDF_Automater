308c4775763102268c023618d1d9fcdb8825cf9b
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-02-25 12:11:57 +0200

    fix: correct date format in cost.py for current and last date

--- sections_functions/cost.py
+++ sections_functions/cost.py
@@ -159,12 +159,12 @@ def print_cost_cover(project_id, url):
             
             properties = payment["properties"]
 
-            property_000 = next(iter([p for p in properties if "000" in p["name"]]), None)
-            property_001 = next(iter([p for p in properties if "001" in p["name"]]), None)
-            property_002 = next(iter([p for p in properties if "002" in p["name"]]), None)
-            property_003 = next(iter([p for p in properties if "003" in p["name"]]), None)
-            property_004 = next(iter([p for p in properties if "004" in p["name"]]), None)
-            property_005 = next(iter([p for p in properties if "005" in p["name"]]), None)
+            property_000 = next(iter([p for p in properties if "000" in p["name"]]), {})
+            property_001 = next(iter([p for p in properties if "001" in p["name"]]), {})
+            property_002 = next(iter([p for p in properties if "002" in p["name"]]), {})
+            property_003 = next(iter([p for p in properties if "003" in p["name"]]), {})
+            property_004 = next(iter([p for p in properties if "004" in p["name"]]), {})
+            property_005 = next(iter([p for p in properties if "005" in p["name"]]), {})
 
 
 
