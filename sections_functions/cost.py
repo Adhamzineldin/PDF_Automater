02c8187587c7103633b540b019f5bf8169dae289
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-02-25 12:15:11 +0200

    fix: update print statement to display change orders instead of cost payments

--- sections_functions/cost.py
+++ sections_functions/cost.py
@@ -85,7 +85,7 @@ def print_cost_cover(project_id, url):
     print(f"cost id is {cost_id}")
     print(len(cost_payments))
     change_orders = [change_order for change_order in change_order_response if change_order["contractId"] in [cost_payment["associationId"] for cost_payment in cost_payments]]
-    print(cost_payments)
+    print(change_orders)
     
     
     
